70031efdbdb15c8cdbdd8f1f43cf043775cd7a64
ADAM Pauline <pauline92@hotmail.fr>
2020-11-10 22:27:07 +0100

    Update

---
+++ src/Case.java
@@ -0,0 +1,22 @@
+public class Case {
+    
+    private int is;
+
+    public Case(char est){
+        is = est;
+    }
+    
+    public int getIs(){
+        return is;
+    }
+    
+    public void setIs(int a){
+        is = a;
+    }
+    
+    //0 = jaune
+    //1 = violet
+    //2 = bleu
+    //3 = vert
+    
+}
---
+++ src/Niveau.java
@@ -0,0 +1,12 @@
+public class Niveau {
+    
+    private Grille grid;
+    public int id;
+    public String name;
+
+    public Niveau(Grille grille, int numero, String nom){
+        grid = grille;
+        id = numero;
+        name = nom;
+    }
+}
