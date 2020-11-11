9ccd64a394f72e1821cd1b4317f458e6ce29f763
ADAM Pauline <pauline92@hotmail.fr>
2020-11-11 21:40:48 +0100

    Implémentation d'un systeme de point lors de la suppression, et d'un systeme de vérification d'un coup valide (plus d'un bloc à supprimer)

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,15 @@
 public class Game {
     
     public static void main(String[] args){
-        Case[][] cas = new Case[4][6];
+        Case[][] cas = new Case[6][8];
         Grille a = new Grille(cas);
         a.remplirBlocs();
         a.afficher();
         a.supprimer(2,4);
+        System.out.println(a.coupValide(2,4));
         a.afficher();
+        System.out.println(a.ontEteSupprime());
+        System.out.println(a.points());
         a.remplacer();
         a.afficher();
     }
