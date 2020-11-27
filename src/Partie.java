10fb8568d8ab4108bfe84c96f073cc6c1909ef26
TORIS ugo <toris.ugo@gmail.com>
2020-11-27 20:24:14 +0100

    update

---
+++ src/Partie.java
@@ -0,0 +1,13 @@
+public class Partie {
+    private Joueur personne;
+    private Grille gril;
+
+    public Partie(Joueur pers, Grille grille){
+        personne = pers;
+        gril = grille;
+    }
+
+    public void newName(String str){
+
+    }
+}
