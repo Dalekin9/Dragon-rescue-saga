7465b280650a91261fefa471b710aeae4c7bff8f
ADAM Pauline <pauline92@hotmail.fr>
2020-12-12 03:13:06 +0100

    5 niveaux realises

--- src/Game.java
+++ src/Game.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 public class Game {
     
     public static Niveau[] init(){
-        Niveau[] tab = new Niveau[2];
-        tab[0] = new Niveau(new Grille(new Case[7][7]),1,2,0,0,new int[][]{{0,1},{0,5}},null, new int[][]{{0,0},{0,2},{0,3},{0,4},{0,6}});
-        tab[1] = new Niveau(new Grille(new Case[8][5]),2,5,0,0,new int[][]{{0,2},{1,2},{2,2},{1,0},{1,4}},null, new int[][]{{0,0},{0,4}});
-        tab[2] = new Niveau(new Grille(new Case[9][7]),3,3,0,0,new int[][]{{0,2},{0,4},{0,6}},null, new int[][]{{0,0},{0,1},{1,0},{1,1},{2,0},{2,1},{3,0},{3,1},{0,3},{0,5},{7,5},{7,6},{8,5},{8,6}});
+        Niveau[] tab = new Niveau[5];
+        tab[0] = new Niveau(new Grille(new Case[7][7]),1,2,0,0);
+        tab[1] = new Niveau(new Grille(new Case[8][5]),2,5,0,0);
+        tab[2] = new Niveau(new Grille(new Case[9][7]),3,3,0,0);
+        tab[3] = new Niveau(new Grille(new Case[8][9]),4,12,0,0);
+        tab[4] = new Niveau(new Grille(new Case[8][9]),5,5,0,0);
         return tab;
     }
     
@@ -29,16 +31,9 @@ public class Game {
             //lecture des niveaux sur le fichier level.ser
             while (fis.available() > 0) {
                 Niveau lvl = (Niveau) ois.readObject ();
-                lvl.setGrid(lvl.remplir_Grille());
-                System.out.println("Niveau : ");
-                System.out.println("id : " + lvl.id);
-                System.out.println("animaux : " + lvl.getNb_animaux());
-                for (int i=0;i<lvl.getGrid().gril.length;i++){
-                    for (int j=0;j<lvl.getGrid().gril[i].length;j++){
-                        System.out.print(lvl.getGrid().gril[i][j].getIs() + " ");
-                    }
-                    System.out.println();
-                }
+                lvl.remplir_Grille();
+                lvl.afficher();
+                lvl.getGrid().afficher();
             }
         } catch (final IOException | ClassNotFoundException e) {
             e.printStackTrace();
--- src/Grille.java
+++ src/Grille.java
@@ -10,111 +10,6 @@ public class Grille implements Serializable {
         gril = grid;
     }
     
-    //on suppose les coordonnées x et y toujours corrects
-    //rempli le tableau avec les animaux
-    public void poserAnimaux(int[][] tab){
-        for (int[] ints : tab) {
-            gril[ints[0]][ints[1]] = new Case('a');
-        }
-    }
-    
-    //on suppose les coordonnées x et y toujours corrects
-    //rempli le tableau de bloc fixe
-    public void poserFixe(int[][] tab){
-        for (int[] ints : tab) {
-            gril[ints[0]][ints[1]] = new Case('n');
-        }
-    }
-    
-    //on suppose les coordonnées x et y toujours corrects
-    //rempli le tableau d'endroit vide
-    public void poserVide(int[][] tab){
-        for (int[] ints : tab) {
-            gril[ints[0]][ints[1]] = new Case('v');
-        }
-    }
-    
-    //rempli le tableau de bloc de couleur
-    public void remplirBlocs(){
-        for(int i=0;i<gril.length;i++){
-            for(int j = 0; j<gril[i].length;j++) {
-                Random rand = new Random();
-                gril[i][j] = new Case((char)((rand.nextInt(4)+1)+'0'));
-            }
-        }
-    }
-    
-    public int chiffre_aleatoire(int longueur){
-        Random rand = new Random();
-        return rand.nextInt(longueur);
-    }
-    
-    public ArrayList<Character> liste(){
-        ArrayList<Character> chiffre = new ArrayList<Character>();
-        chiffre.add('J'); chiffre.add('O'); chiffre.add('R'); chiffre.add('B'); chiffre.add('V');
-        return chiffre;
-    }
-    
-    //rempli si le Niveau est le 1er (car démo)
-    public void remplir_Niveau_1(){
-        ArrayList<Character> chiffre = this.liste();
-        for (int i=0;i<5;i++){
-            int pos = this.chiffre_aleatoire(chiffre.size());
-            if (i==0){
-                gril[1][2] = gril[1][3] = gril[1][4] = gril[3][0] = gril[3][1] = gril[4][0] = gril[4][1] = gril[5][3] = gril[6][3] = gril[5][6] = gril[6][6]= new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else if (i == 1){
-                gril[1][5] = gril[1][6] = gril[2][5] = gril[2][6] = gril[5][1] = gril[5][2] = gril[6][1]  = gril[6][2]= new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else if (i== 2){
-                gril[2][2] = gril[2][3] = gril[2][4] = gril[3][2] = gril[3][3] = gril[3][4] = gril[4][2] = gril[4][3] = gril[4][4] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else if (i== 3){
-                gril[1][0] = gril[1][1] = gril[2][0] = gril[2][1] = gril[5][0] = gril[6][0] = gril[5][4] = gril[5][5] = gril[6][4] = gril[6][5] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else {
-                gril[3][5] = gril[3][6] = gril[4][5] = gril[4][6] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            }
-        }
-    }
-    
-    //rempli si le Niveau est le 2eme (car démo)
-    public void remplir_Niveau_2(){
-        ArrayList<Character> chiffre = this.liste();
-        for (int i=0;i<3;i++){
-            int pos = this.chiffre_aleatoire(chiffre.size());
-            if (i==0){
-                gril[0][1] = gril[0][3] = gril[1][1] = gril[1][3] = gril[4][1] = gril[4][3] = gril[5][1] = gril[5][3] = gril[6][0] = gril[6][4] = gril[7][0] = gril[7][4]= new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else if (i == 1){
-                gril[2][0] = gril[2][4] = gril[3][0] = gril[3][4] = gril[3][2] = gril[4][2] = gril[5][2] = gril[6][2]  = gril[7][2]= new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else {
-                gril[2][1] = gril[2][3] = gril[3][1] = gril[3][3] = gril[4][0] = gril[4][4] = gril[5][0] = gril[5][4] = gril[6][1] = gril[6][3] = gril[7][1] = gril[7][3] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            }
-        }
-    }
-    
-    //rempli si le Niveau est le 3eme (car démo)
-    public void remplir_Niveau_3(){
-        ArrayList<Character> chiffre = this.liste();
-        for (int i=0;i<3;i++){
-            int pos = this.chiffre_aleatoire(chiffre.size());
-            if (i==0){
-                gril[1][3] = gril[2][3] = gril[3][2] = gril[4][2] = gril[4][0] = gril[6][0] = gril[7][2] = gril[8][2] = gril[7][4] = gril[8][4] = gril[1][5] = gril[2][5] = gril[3][5] = gril[4][5] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else if (i == 1){
-                gril[1][2] = gril[2][2] = gril[3][4] = gril[4][4] = gril[3][6] = gril[4][6] = gril[5][0] = gril[5][1]  = gril[5][3] = gril[5][5] = gril[6][1] = gril[6][3] = gril[6][5] = gril[7][0] = gril[7][1] = gril[8][0] = gril[8][1] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            } else {
-                gril[1][4] = gril[1][6] = gril[2][4] = gril[2][6] = gril[3][3] = gril[4][3] = gril[4][1] = gril[5][2] = gril[6][2] = gril[5][4] = gril[6][4] = gril[5][6] = gril[6][6] = gril[7][3] = gril[8][3] = new Case(chiffre.get(pos));
-                chiffre.remove(pos);
-            }
-        }
-    }
-    
     public void afficher(){
         for (Case[] cases : gril) {
             for (Case aCase : cases) {
@@ -123,6 +18,26 @@ public class Grille implements Serializable {
             System.out.println();
         }
         System.out.println();
+    }
+    
+    //---------------------------------------------------------
+    //              --- PARTIE 2 ---                          -
+    //       gestions des coups dans une partie               -
+    //---------------------------------------------------------
+    
+    public boolean coupValide(int x,int y){
+        int res = gril[x][y].getIs();
+        if (x-1 > -1 && gril[x-1][y].getIs() == res){
+            return true;
+        } else if (x+1 < gril.length && gril[x+1][y].getIs() == res){
+            return true;
+        } else if (y-1 > -1 && gril[x][y-1].getIs() == res){
+            return true;
+        } else if (y+1 < gril.length && gril[x][y+1].getIs() == res){
+            return true;
+        } else {
+            return false;
+        }
     }
     
     public boolean remplie(){
@@ -187,19 +102,109 @@ public class Grille implements Serializable {
         return compt*compt*10;
     }
     
-    public boolean coupValide(int x,int y){
-        int res = gril[x][y].getIs();
-        if (x-1 > -1 && gril[x-1][y].getIs() == res){
-            return true;
-        } else if (x+1 < gril.length && gril[x+1][y].getIs() == res){
-            return true;
-        } else if (y-1 > -1 && gril[x][y-1].getIs() == res){
-            return true;
-        } else if (y+1 < gril.length && gril[x][y+1].getIs() == res){
-            return true;
-        } else {
-            return false;
+    
+    //---------------------------------------------------------
+    //              --- PARTIE 2 ---                          -
+    //         gestion liée à la fin du jeu                   -
+    //---------------------------------------------------------
+    
+    
+    //---------------------------------------------------------
+    //              --- PARTIE 2 ---                          -
+    //       affichage du niveau et des scores                -
+    //---------------------------------------------------------
+    
+    public void remplir_Niveau_1(ArrayList<Character> liste){
+        for (int i=0;i<5;i++){
+            if ((5-i-1) == 4){
+                gril[1][2] = gril[1][3] = gril[1][4] = gril[3][0] = gril[3][1] = gril[4][0] = gril[4][1] = gril[5][3] = gril[6][3] = gril[5][6] = gril[6][6]= new Case(liste.get(4));
+                liste.remove(4);
+            } else if ((5-i-1) == 3){
+                gril[1][5] = gril[1][6] = gril[2][5] = gril[2][6] = gril[5][1] = gril[5][2] = gril[6][1]  = gril[6][2]= new Case(liste.get(3));
+                liste.remove(3);
+            } else if ((5-i-1) == 2){
+                gril[2][2] = gril[2][3] = gril[2][4] = gril[3][2] = gril[3][3] = gril[3][4] = gril[4][2] = gril[4][3] = gril[4][4] = new Case(liste.get(2));
+                liste.remove(2);
+            } else if ((5-i-1) == 1){
+                gril[1][0] = gril[1][1] = gril[2][0] = gril[2][1] = gril[5][0] = gril[6][0] = gril[5][4] = gril[5][5] = gril[6][4] = gril[6][5] = new Case(liste.get(1));
+                liste.remove(1);
+            } else {
+                gril[3][5] = gril[3][6] = gril[4][5] = gril[4][6] = new Case(liste.get(0));
+            }
+            //pose les animaux
+            gril[0][1] = gril[0][5] = new Case('a');
+            //pose les blocs vide du depart
+            gril[0][0] = gril[0][2] = gril[0][3] = gril[0][4] = gril[0][6] = new Case(' ');
         }
+    }
+    
+    
+    public void remplir_Niveau_2(ArrayList<Character> liste){
+        for (int i=0;i<3;i++){
+            if ((3-i-1)==2){
+                gril[0][1] = gril[0][3] = gril[1][1] = gril[1][3] = gril[4][1] = gril[4][3] = gril[5][1] = gril[5][3] = gril[6][0] = gril[6][4] = gril[7][0] = gril[7][4]= new Case(liste.get(2));
+                liste.remove(2);
+            }
+            else if ((3-i-1) == 1){
+                gril[2][0] = gril[2][4] = gril[3][0] = gril[3][4] = gril[3][2] = gril[4][2] = gril[5][2] = gril[6][2]  = gril[7][2]= new Case(liste.get(1));
+                liste.remove(1);
+            } else {
+                gril[2][1] = gril[2][3] = gril[3][1] = gril[3][3] = gril[4][0] = gril[4][4] = gril[5][0] = gril[5][4] = gril[6][1] = gril[6][3] = gril[7][1] = gril[7][3] = new Case(liste.get(0));
+            }
+            //pose les animaux
+            gril[0][2] = gril[1][2] = gril[2][2] = gril[1][0] = gril[1][4] = new Case('a');
+            //pose les blocs vides
+            gril[0][0] = gril[0][4] = new Case (' ');
+        }
+    }
+    
+    public void remplir_Niveau_3(ArrayList<Character> liste){
+        for (int i=0;i<3;i++){
+            if ((3-i-1)==2){
+                gril[1][3] = gril[2][3] = gril[3][2] = gril[4][2] = gril[4][0] = gril[6][0] = gril[7][2] = gril[8][2] = gril[7][4] = gril[8][4] = gril[1][5] = gril[2][5] = gril[3][5] = gril[4][5] = new Case(liste.get(2));
+                liste.remove(2);
+            } else if ((3-i-1) == 1){
+                gril[1][2] = gril[2][2] = gril[3][4] = gril[4][4] = gril[3][6] = gril[4][6] = gril[5][0] = gril[5][1]  = gril[5][3] = gril[5][5] = gril[6][1] = gril[6][3] = gril[6][5] = gril[7][0] = gril[7][1] = gril[8][0] = gril[8][1] = new Case(liste.get(1));
+                liste.remove(1);
+            } else {
+                gril[1][4] = gril[1][6] = gril[2][4] = gril[2][6] = gril[3][3] = gril[4][3] = gril[4][1] = gril[5][2] = gril[6][2] = gril[5][4] = gril[6][4] = gril[5][6] = gril[6][6] = gril[7][3] = gril[8][3] = new Case(liste.get(0));
+            }
+            //pose les animaux
+            gril[0][2] = gril[0][4] = gril[0][6] = new Case('a');
+            //pose les blocs vides
+            gril[0][0] = gril[0][1] = gril[1][0] = gril[1][1] = gril[2][0] = gril[2][1] = gril[3][0] = gril[3][1] = gril[0][3] = gril[0][5] =  new Case(' ');
+            //pose les blocs fixes
+            gril[7][5] = gril[7][6] = gril[8][5] = gril[8][6] = new Case('-');
+        }
+    }
+    
+    public void remplir_Niveau_4(ArrayList<Character> liste){
+        for (int i=0;i<8;i++){
+            for (int j=0;j<9;j++){
+                gril[i][j] = new Case (liste.get((int) (Math.random()*3)));
+            }
+        }
+        //pose les animaux
+        gril[3][1] = gril[3][3] = gril[3][5] = gril[3][7] = new Case('a');
+    }
+    
+    public void remplir_Niveau_5(ArrayList<Character> liste){
+        //pose les blocs de couleur supérieur
+        for (int i=0;i<4;i++){
+            for (int j=0;j<9;j++){
+                gril[i][j] = new Case (liste.get((int) (Math.random()*3)));
+            }
+        }
+        //pose les blocs de couleur inférieur
+        gril[6][2] = gril[7][3] = gril[6][4] = gril[7][5] = gril[6][6] = new Case(liste.get(0));
+        gril[7][2] = gril[6][3] = gril[7][4] = gril[6][5] = gril[7][6] = new Case(liste.get(1));
+        //pose les animaux
+        gril[5][2] = gril[5][3] = gril[5][4] = gril[5][5] = gril[5][6] = new Case('a');
+        //pose les blocs fixes
+        gril[3][0] = gril[3][8] =gril[5][1] = gril[5][7] = gril[6][1] = gril[6][7] = gril[7][1] = gril[7][7]
+                = gril[4][0] = gril[4][1] = gril[4][2] = gril[4][3] = gril[4][4]= gril[4][5] = gril[4][6] = gril[4][7] = gril[4][8] = new Case('-');
+        //pose les emplacements vides
+        gril[0][0] = gril[0][8] = gril[5][0] = gril[5][8] = gril[6][0] = gril[6][8] = gril[7][0] = gril[7][8] = new Case(' ');
     }
     
 }
