45b5f8cad6ad6a3f3fa341c9261f5f31402e11f6
ADAM Pauline <pauline92@hotmail.fr>
2020-12-23 19:38:34 +0100

    Coup special bloc fonctionnel

--- src/Niveau.java
+++ src/Niveau.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Niveau implements Serializable {
-    
+
     private Grille grid;
     public int id;
     protected int nb_animaux;
@@ -20,7 +20,7 @@ public class Niveau implements Serializable {
         nb_point_min = point;
         best_score = new int[]{0, 0, 0, 0, 0};
     }
-    
+
     //remplissage des grilles selon le niveau
     public void remplir_Grille(){
         if (id == 1){
@@ -35,25 +35,25 @@ public class Niveau implements Serializable {
             grid.remplir_Niveau_5(this.liste(3));
         }
     }
-    
-    
+
+
     //---------------------------------------------------------
     //                   --- PARTIE 2 ---                     -
     //                  liste de couleurs                     -
     //---------------------------------------------------------
-    
-    
+
+
     public int chiffre_aleatoire(int longueur){
         Random rand = new Random();
         return rand.nextInt(longueur);
     }
-    
+
     public ArrayList<Character> listeColor(){
         ArrayList<Character> chiffre = new ArrayList<Character>();
         chiffre.add('J'); chiffre.add('O'); chiffre.add('R'); chiffre.add('B'); chiffre.add('V');
         return chiffre;
     }
-    
+
     public ArrayList<Character> liste(int longueur){
         ArrayList<Character> chiffre = listeColor();
         ArrayList<Character> fin = new ArrayList<Character>();
@@ -64,13 +64,13 @@ public class Niveau implements Serializable {
         }
         return fin;
     }
-    
-    
+
+
     //---------------------------------------------------------
     //              --- PARTIE 2 ---                          -
     //       affichage du niveau et des scores                -
     //---------------------------------------------------------
-    
+
     //affichage des propriétés du niveau en mode texte
     public void afficher(){
         System.out.println("Niveau "+id);
@@ -84,7 +84,7 @@ public class Niveau implements Serializable {
         }
         afficher_score();
     }
-    
+
     public void afficher_score(){
         System.out.println("Meilleurs scores :");
         for (int i=0;i<5;i++){
@@ -95,21 +95,21 @@ public class Niveau implements Serializable {
             }
         }
     }
-    
-    
+
+
     //---------------------------------------------------------
     //              --- PARTIE 3 ---                          -
     //      contient les getters (et les setters)             -
     //---------------------------------------------------------
-    
+
     public Grille getGrid() {
         return grid;
     }
-    
+
     public int getNb_animaux() {
         return nb_animaux;
     }
-    
+
     public int getNb_coup_min() {
         return nb_coup_min;
     }
