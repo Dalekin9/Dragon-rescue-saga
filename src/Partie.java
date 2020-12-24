3e8f869a31433d12fdbb2a33e652fff6033be762
TORIS ugo <toris.ugo@gmail.com>
2020-12-24 16:09:50 +0100

    Classe partie partiellement finie

--- src/Partie.java
+++ src/Partie.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 public class Partie {
     private Joueur joueur;
     private Niveau lvl;
-    private int score;
+    private int score,coupRes,animRes;
 
     public Partie(Joueur pers, Niveau niveau){
         joueur = pers;
         lvl = niveau;
         score = 0;
+        coupRes = lvl.getNb_coup_max();
+        animRes = lvl.nb_animaux;
     }
 
     public int[] coordsInt(String[] coords){
@@ -23,24 +25,25 @@ public class Partie {
         int hauteur = lvl.getGrid().getHaut();
         ArrayList nmbrs = new ArrayList();
         ArrayList letters = new ArrayList();
-        for(int i = 0; i<hauteur; i++)nmbrs.add(i);
-        for(int j = 0; j<longueur; j++)letters.add('a'+j);
+        for(int i = 0; i<hauteur; i++)nmbrs.add(String.valueOf(i));
+        for(int j = 0; j<longueur; j++)letters.add(String.valueOf((char)('a'+ j)));
         if(nmbrs.contains(coords[0]) && letters.contains(coords[1].toLowerCase())){
-            for(int i = 0; i<longueur; i++) {
-                if (letters.get(i).equals(coords[1].toLowerCase())) {
-                    coords[1] = Integer.toString(i);
+            for(int k = 0; k<longueur; k++) {
+                if (letters.get(k).equals(coords[1].toLowerCase())) {
+                    coords[1] = Integer.toString(k);
                 }
             }
             String tmp = coords[0];
             coords[0]  = coords[1];
             coords[1]  = tmp;
         }else if(nmbrs.contains(coords[1]) && letters.contains(coords[0].toLowerCase())){
-            for(int i = 0; i<longueur; i++) {
-                if (letters.get(i).equals(coords[0].toLowerCase())) {
-                    coords[0] = Integer.toString(i);
+            for(int k = 0; k<longueur; k++) {
+                if (letters.get(k).equals(coords[0].toLowerCase())) {
+                    coords[0] = Integer.toString(k);
                 }
             }
         }else{
+            System.out.println("Les coordonées ne sont pas valides");
             return false;
         }
         return true;
@@ -48,7 +51,7 @@ public class Partie {
 
 
 
-    public void uneAction(){
+    public void uneAction(boolean animAlea){
         System.out.println("Voulez-vous supprimer un bloc ou utiliser un objet? (B(loc)/O(bjet))");
         boolean flag = false;
         String[] coordsStr;
@@ -64,16 +67,45 @@ public class Partie {
                             flag = true;
                         }
                     }while(!flag);
-                    lvl.grid.supprimer(coords[0],coords[1]);
+                    lvl.getGrid().supprimer(coords[0],coords[1]);
+                    score += lvl.getGrid().points();
+                    lvl.getGrid().faireDescendre(animAlea);
+                    if(lvl.getGrid().animalEnBas()){
+                        int  ajout = lvl.getGrid().supprimerAnimalEnBas();
+                        score += ajout;
+                        animRes -= ajout/1000;
+                    }
+                    lvl.getGrid().faireDescendre(animAlea);
+                    lvl.getGrid().decaler();
+                    coupRes--;
                     break;
                 case "O":
                 case "Objet":
                     flag = true;
+                    coupRes--;
                     break;
                 default:
                     System.out.println("Réponse non reconnue, choisissez B ou O");
                     break;
             }
         }while(!flag);
+    }
+
+    public void jouer(){
+        lvl.afficher();
+        do{
+            lvl.getGrid().afficher();
+            uneAction(false);
+        }while(finJeu() == 0);
+    }
+
+    public int finJeu(){
+        if(coupRes == 0){
+            return 1;
+        }else if(animRes == 0 || score >= lvl.nb_point_min){
+            return 2;
+        }else{
+            return 0;
+        }
     }
 }
