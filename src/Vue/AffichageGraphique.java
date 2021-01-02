6e3f20620d1933044f8babd43e34e7c3e6a6abec
ADAM Pauline <pauline92@hotmail.fr>
2021-01-02 19:34:11 +0100

    interface debut oke

--- src/Vue/AffichageGraphique.java
+++ src/Vue/AffichageGraphique.java
@@ -13,6 +13,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.time.chrono.IsoChronology;
 import java.util.HashMap;
@@ -30,8 +32,9 @@ public class AffichageGraphique extends JFrame {
     public AffichageGraphique(Controleur controleur){
         control = controleur;
         setTitle("Bear Rescue Saga");
-        setSize(550,650);
+        setSize(550,750);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
     }
 
     //setter et getter
@@ -49,230 +52,242 @@ public class AffichageGraphique extends JFrame {
     //
 
     public void ecranCo(){
-        //création d'un panneau pour l'ajouter à la Vue
-        JPanel pan = new JPanel(new GridBagLayout());
 
-        //mise à jour des contraintes
+        JLabel jLabel1 = new JLabel();
+
+        JButton jButton3 = new JButton();
+        JButton jButton4 = new JButton();
+        JLabel jLabel3 = new JLabel();
+
+        JPanel princ = new JPanel();
+        princ.setOpaque(false);
+        princ.setLayout(null);
+
+        JPanel sec = new JPanel();
+        sec.setOpaque(false);
+        sec.setLayout(new java.awt.GridBagLayout());
+
+
+        jLabel1.setText("<html><h1><strong>Rescue All Dragons</strong></h1><hr></html>");
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.anchor = GridBagConstraints.CENTER;
         gbc.insets = new Insets(30,0,0,0);
+        sec.add(jLabel1, gbc);
 
-        pan.add(new JLabel("<html><h1><strong>Bear Rescue Saga</strong></h1><hr></html>"), gbc);
+        JPanel butt= new JPanel();
+        butt.setOpaque(false);
+        butt.setLayout(new GridBagLayout());
 
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(5, 0, 5, 0);
 
-        //création du panneau des boutons
-        JPanel buttons = new JPanel(new GridBagLayout());
-
-        //création des boutons et de leur évènement lié
-        JButton av = new JButton("Se connecter");
-        av.addActionListener(new ActionListener() {
+        jButton3.setText("Connexion");
+        jButton3.setPreferredSize(new Dimension(150,55));
+        jButton3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 control.connexion();
             }
         });
+        butt.add(jButton3, gbc);
 
-        JButton inf = new JButton("S'inscrire");
-        inf.addActionListener(new ActionListener() {
+        jButton4.setText("Incription");
+        jButton4.setPreferredSize(new Dimension(150,55));
+        jButton4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                control.inscription();
+                control.connexion();
             }
         });
-
-        //ajout des boutons au panneau de boutons
-        buttons.add(av, gbc);
-        buttons.add(inf, gbc);
+        butt.add(jButton4, gbc);
 
         gbc.weighty = 1;
-        pan.add(buttons, gbc);
-        main.add("Ecran de connexion",pan);
-        cl.show(main,"Ecran de connexion");
-        add(main);
-        main.setVisible(true);
+
+        sec.add(butt,gbc);
+
+        princ.add(sec);
+        sec.setBounds(0, 0, 550, 750);
+
+        jLabel3.setIcon(new ImageIcon("C:\\Users\\pauli\\bear-rescuse-saga\\src\\Images\\fond3.png"));
+        princ.add(jLabel3);
+        jLabel3.setBounds(0, 0, 550, 750);
+
+        main.add(princ, "accueil");
+
+        getContentPane().add(main);
+        main.setBounds(0, 0, 550, 750);
+        setVisible(true);
     }
 
     public void connexion() {
-        JPanel panneauCo = new JPanel(new GridBagLayout());
+
+        JLabel jLabel1 = new JLabel();
+
+        JButton jButton3 = new JButton();
+        JButton jButton4 = new JButton();
+        JLabel jLabel3 = new JLabel();
+
+        JPanel princ = new JPanel();
+        princ.setOpaque(false);
+        princ.setLayout(null);
+
+        JPanel sec = new JPanel();
+        sec.setOpaque(false);
+        sec.setLayout(new java.awt.GridBagLayout());
+
+        jLabel1.setText("<html><h1><strong>Connexion</strong></h1><hr></html>");
+
         GridBagConstraints gbc = new GridBagConstraints();
-        JPanel panneauButt = new JPanel(new GridBagLayout());
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridy = 1;
+        gbc.gridx=1;
+        sec.add(jLabel1, gbc);
+
+        JPanel butt= new JPanel();
+        butt.setOpaque(false);
+        butt.setLayout(new GridBagLayout());
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 0, 5, 0);
 
         JTextField ident = new JTextField("identifiant");
-        ident.getFont().deriveFont(Font.ITALIC);
-        ident.setForeground(Color.gray);
-        ident.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-            @Override
-            public void mousePressed(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JTextField texteField = ((JTextField)e.getSource());
-                texteField.setText("");
-                texteField.getFont().deriveFont(Font.PLAIN);
-                texteField.setForeground(Color.black);
-                texteField.removeMouseListener(this);
-            }});
         ident.setPreferredSize(new Dimension(150,30));
+        ident.setHorizontalAlignment(JTextField.CENTER);
+        gbc.gridx = 3;
+        gbc.gridx++;
+        gbc.gridy++;
+        butt.add(ident,gbc);
 
         JTextField pswd = new JTextField("mot de passe");
-        pswd.getFont().deriveFont(Font.ITALIC);
-        pswd.setForeground(Color.gray);
-        pswd.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-            @Override
-            public void mousePressed(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JTextField texteField = ((JTextField)e.getSource());
-                texteField.setText("");
-                texteField.getFont().deriveFont(Font.PLAIN);
-                texteField.setForeground(Color.black);
-                texteField.removeMouseListener(this);
-            }});
         pswd.setPreferredSize(new Dimension(150,30));
+        pswd.setHorizontalAlignment(JTextField.CENTER);
+        gbc.gridy++;
+        butt.add(pswd,gbc);
 
-        JButton connex = new JButton("Confirmer");
-        connex.addActionListener(new ActionListener() {
-            @Override
+        jButton3.setText("Confirmer");
+        jButton3.setPreferredSize(new Dimension(150,30));
+        jButton3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 control.seConnecter(ident.getText(),pswd.getText());
             }
         });
+        gbc.insets = new Insets(10,0,0,0);
+        gbc.gridy++;
+        butt.add(jButton3, gbc);
 
-        JButton av = new JButton("S'inscrire");
-        av.addActionListener(new ActionListener() {
+        jButton4.setText("Incription");
+        jButton4.setPreferredSize(new Dimension(150,30));
+        jButton4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 control.inscription();
             }
         });
+        gbc.gridy++;
+        butt.add(jButton4, gbc);
 
-        gbc.gridx=1;
-        gbc.insets = new Insets(10,0,0,0);
-        gbc.gridy=1;
-        panneauButt.add(ident,gbc);
-        gbc.gridy=2;
-        panneauButt.add(pswd,gbc);
-        gbc.gridy=3;
-        panneauButt.add(connex,gbc);
-        gbc.gridy=5;
-        panneauButt.add(av,gbc);
-        panneauCo.add(panneauButt);
-        main.add("connexion",panneauCo);
+        gbc.anchor = GridBagConstraints.LINE_END;
+        sec.add(butt,gbc);
+
+        princ.add(sec);
+        sec.setBounds(0, 0, 550, 750);
+
+        jLabel3.setIcon(new ImageIcon("C:\\Users\\pauli\\bear-rescuse-saga\\src\\Images\\connexion.png"));
+        princ.add(jLabel3);
+        jLabel3.setBounds(0, 0, 550, 750);
+
+        main.add(princ, "connexion");
         cl.show(main,"connexion");
+
     }
 
     public void inscription(){
-        JPanel panneauCo = new JPanel(new GridBagLayout());
+
+        JLabel jLabel1 = new JLabel();
+
+        JButton jButton3 = new JButton();
+        JButton jButton4 = new JButton();
+        JLabel jLabel3 = new JLabel();
+
+        JPanel princ = new JPanel();
+        princ.setOpaque(false);
+        princ.setLayout(null);
+
+        JPanel sec = new JPanel();
+        sec.setOpaque(false);
+        sec.setLayout(new java.awt.GridBagLayout());
+
+
+        jLabel1.setText("<html><h1><strong>Inscription</strong></h1><hr></html>");
+
         GridBagConstraints gbc = new GridBagConstraints();
-        JPanel panneauButt = new JPanel(new GridBagLayout());
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridy = 1;
+        gbc.gridx=1;
+        sec.add(jLabel1, gbc);
+
+        JPanel butt= new JPanel();
+        butt.setOpaque(false);
+        butt.setLayout(new GridBagLayout());
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 0, 5, 0);
 
         JTextField ident = new JTextField("identifiant");
-        ident.getFont().deriveFont(Font.ITALIC);
-        ident.setForeground(Color.gray);
-        ident.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-            @Override
-            public void mousePressed(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JTextField texteField = ((JTextField)e.getSource());
-                texteField.setText("");
-                texteField.getFont().deriveFont(Font.PLAIN);
-                texteField.setForeground(Color.black);
-                texteField.removeMouseListener(this);
-            }});
         ident.setPreferredSize(new Dimension(150,30));
+        ident.setHorizontalAlignment(JTextField.CENTER);
+        gbc.gridx = 3;
+        gbc.gridx++;
+        gbc.gridy++;
+        butt.add(ident,gbc);
 
         JTextField pswd = new JTextField("mot de passe");
-        pswd.getFont().deriveFont(Font.ITALIC);
-        pswd.setForeground(Color.gray);
-        pswd.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-            @Override
-            public void mousePressed(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JTextField texteField = ((JTextField)e.getSource());
-                texteField.setText("");
-                texteField.getFont().deriveFont(Font.PLAIN);
-                texteField.setForeground(Color.black);
-                texteField.removeMouseListener(this);
-            }});
         pswd.setPreferredSize(new Dimension(150,30));
+        pswd.setHorizontalAlignment(JTextField.CENTER);
+        gbc.gridy++;
+        butt.add(pswd,gbc);
 
         JTextField pswd2 = new JTextField("mot de passe");
-        pswd2.getFont().deriveFont(Font.ITALIC);
-        pswd2.setForeground(Color.gray);
-        pswd2.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-            @Override
-            public void mousePressed(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JTextField texteField = ((JTextField)e.getSource());
-                texteField.setText("");
-                texteField.getFont().deriveFont(Font.PLAIN);
-                texteField.setForeground(Color.black);
-                texteField.removeMouseListener(this);
-            }});
         pswd2.setPreferredSize(new Dimension(150,30));
+        pswd2.setHorizontalAlignment(JTextField.CENTER);
+        gbc.gridy++;
+        butt.add(pswd2,gbc);
 
-        JButton inscr = new JButton("Confirmer");
-        inscr.addActionListener(new ActionListener() {
+        jButton3.setText("Confirmer");
+        jButton3.setPreferredSize(new Dimension(150,30));
+        jButton3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 control.sInscrire(ident.getText(),pswd.getText(),pswd2.getText());
             }
         });
+        gbc.insets = new Insets(10,0,0,0);
+        gbc.gridy++;
+        butt.add(jButton3, gbc);
 
-        JButton av = new JButton("Se connecter");
-        av.addActionListener(new ActionListener() {
+        jButton4.setText("Connexion");
+        jButton4.setPreferredSize(new Dimension(150,30));
+        jButton4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 control.connexion();
             }
         });
+        gbc.gridy++;
+        butt.add(jButton4, gbc);
 
-        gbc.gridx=1;
-        gbc.insets = new Insets(10,0,0,0);
-        gbc.gridy=1;
-        panneauButt.add(ident,gbc);
-        gbc.gridy=2;
-        panneauButt.add(pswd,gbc);
-        gbc.gridy=3;
-        panneauButt.add(pswd2,gbc);
-        gbc.gridy=4;
-        panneauButt.add(inscr,gbc);
-        gbc.gridy=5;
-        panneauButt.add(av,gbc);
-        panneauCo.add(panneauButt);
-        main.add("Inscription",panneauCo);
-        cl.show(main,"Inscription");
+        gbc.anchor = GridBagConstraints.LINE_END;
+        sec.add(butt,gbc);
+
+        princ.add(sec);
+        sec.setBounds(0, 0, 550, 750);
+
+        jLabel3.setIcon(new ImageIcon("C:\\Users\\pauli\\bear-rescuse-saga\\src\\Images\\connexion.png"));
+        princ.add(jLabel3);
+        jLabel3.setBounds(0, 0, 550, 750);
+
+        main.add(princ, "inscription");
+        cl.show(main,"inscription");
     }
 
     public void sommaire(){
